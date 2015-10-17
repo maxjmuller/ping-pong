1b0f486f1926ae9affbe3372fe9e833f92433599
Max Muller <maxmuller@gmail.com>
2015-10-16 22:55:06 -0700

    Add javascript enabling button to return correct results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   $("button").click(function() {
-    var number = prompt("Enter any number")
+    var number = prompt("Enter any number");
+    $("p#output").remove();
+    $("#redo").remove();
+    $("button").after("<span id='redo'> (Click here to redo.)</spam>");
+    $("p#button").after("<p id='output'></p>");
+    for (index = 1; index <= number; index +=1) {
+      if (!(index % 3) && !(index % 5)) {
+        $("p#output").append("ping-pong<br>");
+      } else if (!(index % 3)) {
+        $("p#output").append("ping<br>");
+      } else if (!(index % 5)) {
+        $("p#output").append("pong<br>");
+      } else {
+        $("p#output").append(index + "<br>");
+      }
+    }
   });
 });
